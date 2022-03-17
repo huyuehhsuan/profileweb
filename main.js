42fc6ce4252e00436c14d152a1848d2c0b32b436
huyuehhsuan <chow825win878@gmail.com>
2022-03-17 20:50:27 +0800

    style

--- main.js
+++ main.js
@@ -1,21 +1,53 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
-
+const navMenu = document.querySelector(".nav_menu")
+const navToggle = document.querySelector(".nav_toggle")
+const navClose = document.querySelector(".nav_close")
+console.log(navClose);
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
-
+const navLinks = document.querySelectorAll(".nav_link")
+console.log(navLinks);
+navLinks.forEach((navLink) => {
+    navLink.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+})
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContnet = document.getElementsByClassName("skills_content")
+const skillsHeader = document.querySelectorAll(".skills_header")
 
+function skillsToggle() {
 
-/*==================== QUALIFICATION TABS ====================*/
+    let itemClass = this.parentNode.getElementsByClassName
+    console.log(itemClass);
+    for (i = 0; i < skillsContnet.length; i++) {
+        skillsContnet[i].className = "skills_content skills_close"
+    }
+    console.log(skillsContnet);
+    if (itemClass === "skills_content skills_close") {
+        this.parentNode.className = "skills_content skills_open"
+    }
+}
+skillsHeader.forEach((item) => {
+        item.addEventListener("click", skillsToggle)
+    })
+    /*==================== QUALIFICATION TABS ====================*/
 
 
 /*==================== SERVICES MODAL ====================*/
